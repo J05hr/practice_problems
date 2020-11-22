db4e098eeb5df39dd2660137bd57959661960948
J05h <J05h@protonmail.com>
2020-11-22 14:36:47 -0500

    updates

--- CC_Qs/BloombergPractice/bstNodes.py
+++ CC_Qs/BloombergPractice/bstNodes.py
@@ -1,11 +1,65 @@
 # delete a node from BST, given the key and return the updated key in its place.
 
 class Solution:
-    def func(self, param):
-        return None
+    def delNode(self, root, tar):
+        if root is None:
+            return root
+            # initialize basic items
+        parent = None
+        node = root
+
+        # traverse the tree
+        while node and node.val != tar:
+            # record the parent for later
+            parent = node
+
+            # traverse
+            if tar < node.val:
+                node = node.left
+            else:
+                node = node.right
+            # If node is none after traversal then target doesn't exist in the tree
+            if node is None:
+                return root
+
+        # if it has no children just remove it
+        if node.left is None and node.right is None:
+            if node == root:
+                return None
+            else:
+                if parent.left == node:
+                    parent.left = None
+                else:
+                    parent.right = None
+
+        # if it has a right child swap with successor
+        elif node.right is not None:
+            # find successor
+            suc = node.right
+            while suc.left:
+                suc = suc.left
+            val = suc.val
+            # recursively delete the successor
+            node.right = self.delNode(node.right, val)
+            # swap values
+            node.val = val
+
+        # if only left child, swap with predecessor
+        else:
+            # find predecessor
+            pre = node.left
+            while pre.right:
+                pre = pre.right
+            val = pre.val
+            # recursively delete the successor
+            node.left = self.delNode(node.left, val)
+            # swap values
+            node.val = val
+
+        return root
 
 
 if __name__ == '__main__':
     tester = Solution()
-    ans = tester.func(None)
+    ans = tester.delNode([5,3,6,2,4,None,7], 3)
     print(ans)
--- CC_Qs/BloombergPractice/subsets.py
+++ CC_Qs/BloombergPractice/subsets.py
@@ -3,6 +3,32 @@
 
 class Solution:
     def subsets(self, nums):
+
+        def sub(nums, r):
+            if len(r) == 0:
+                for num in nums:
+                    r.append([num])
+
+            for reIdx in range(len(r)):
+                re = r[reIdx]
+                sub = list()
+                sub.append(re[0])
+                for numIdx in range(len(nums)):
+                    num = nums[numIdx]
+                    if num not in sub:
+                        sub.append(num)
+                    prev = re.copy()
+                    if num not in prev:
+                        prev.append(num)
+                    if sub != prev:
+                        if len(sub) > 1:
+                            r.append(sub.copy())
+                        if len(prev) > 1:
+                            r.append(prev)
+                    elif len(prev) > 1:
+                        r.append(prev)
+            return r
+
         # results array
         res = list()
         # always add an empty subset
@@ -11,16 +37,44 @@ class Solution:
         if len(nums) == 0:
             return res
         if len(nums) == 1:
-            return res.append(nums[0])
+            res.append([nums[0]])
+            return res
 
-        for item in nums:
-            subsetarr = []
-
+        r = list()
+        r = sub(nums, r)
+        res.extend(r)
         return res
 
+
+    # cascading
+    # Let's start from empty subset in output list.
+    # At each step one takes new integer into consideration and generates new subsets from the existing ones.
+    def subsets2(self, nums):
+        output = [[]]
+
+        for num in nums:
+            output += [curr + [num] for curr in output]
+
+        return output
+
+
+    def subsets3(self, nums):
+        output = [[]]
+
+        for num in nums:
+            newSub = list()
+            for item in output:
+                a = item.copy()
+                a.append(num)
+                newSub.append(a)
+            for item in newSub:
+                output.append(item)
+
+        return output
 
 
 if __name__ == '__main__':
     tester = Solution()
-    ans = tester.subsets([1,2,3])
-    print(ans)
+    # ans = tester.subsets([1,2,3])
+    ans2 = tester.subsets3([3,2,4,1])
+    print(ans2)
