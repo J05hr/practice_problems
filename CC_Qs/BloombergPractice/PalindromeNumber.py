cd755352208a49be4183ff989d4c3aac7759f758
J05h <J05h@protonmail.com>
2020-11-23 14:57:47 -0500

    updates

--- CC_Qs/BloombergPractice/PalindromeNumber.py
+++ CC_Qs/BloombergPractice/PalindromeNumber.py
@@ -1,12 +1,37 @@
 # https://leetcode.com/problems/palindrome-number/
-
+import math
 
 class Solution:
-    def func(self, param):
-        return None
+    def pal(self, num):
+        # neg arent palandromes
+        if num < 0:
+            return False
+        elif num == 0:
+            return True
+        else:
+            digits = math.floor(math.log10(num)+1)
+            # use modulo or bitmasks to get individual digits
+            # Go through the last half of the digits find their values and check against opposite digit
+            backNum = num
+            frontNum = num
+            halfidx = math.ceil(digits/2)
+            for digit in range(1, halfidx+1):
+                # get the first digit and remove
+                val = backNum % 10
+                backNum = math.floor(backNum/10)
+                # calc the op digit loc
+                opDigit = digits - digit + 1
+                # use division to get the opVal
+                opMask = pow(10, opDigit-1)
+                opVal = math.floor(frontNum / opMask)
+                frontNum = frontNum % opMask
+                # compare
+                if val != opVal:
+                    return False
+        return True
 
 
 if __name__ == '__main__':
     tester = Solution()
-    ans = tester.func(None)
+    ans = tester.pal(1991)
     print(ans)
--- CC_Qs/BloombergPractice/streamOutput.py
+++ CC_Qs/BloombergPractice/streamOutput.py
@@ -2,12 +2,30 @@
 
 
 
+# use minheap
+
+# https://www.geeksforgeeks.org/min-heap-in-java/
+
+"""
+    getMin(): It returns the root element of Min Heap. Time Complexity of this operation is O(1).
+    extractMin(): Removes the minimum element from MinHeap. Time Complexity of this Operation is O(Log n) as this operation needs to maintain the heap property (by calling heapify()) after removing root.
+    insert(): Inserting a new key takes O(Log n) time. We add a new key at the end of the tree. If new key is larger than its parent, then we don’t need to do anything. Otherwise, we need to traverse up to fix the violated heap property.
+"""
+
+
+
+import heapq
+
+
+
 class Solution:
-    def func(self, param):
-        return None
+    def func(self, list):
+        heapq.heapify(list)
+        while len(list) > 0:
+            print(heapq.heappop(list))
 
 
 if __name__ == '__main__':
     tester = Solution()
-    ans = tester.func(None)
+    ans = tester.func(list())
     print(ans)
