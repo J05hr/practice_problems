9ad82049ea2396387113d87565c6a9d7f92cbd02
J05h <35610294+J05hr@users.noreply.github.com>
2020-11-23 00:46:44 -0500

    updates

--- CC_Qs/BloombergPractice/isSorted.py
+++ CC_Qs/BloombergPractice/isSorted.py
@@ -1,8 +1,31 @@
 # Given an array return whether the array is sorted or not (boolean).
 
 class Solution:
-    def func(self, param):
-        return None
+    def isSorted(self, arr):
+
+        if len(arr) < 2:
+            return True
+
+        Asc = False
+        Dsc = False
+        last = None
+
+        for item in arr:
+            if Asc:
+                if last - item < 0:
+                    return False
+            elif Dsc:
+                if last - item > 0:
+                    return False
+            else:
+                if last is not None:
+                    if last - item > 0:
+                        Asc = True
+                    elif last - item < 0:
+                        Dsc = True
+            last = item
+
+        return True
 
 
 if __name__ == '__main__':
