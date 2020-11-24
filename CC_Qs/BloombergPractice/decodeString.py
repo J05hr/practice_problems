994c6ca2a4e19dc2126ee73800108ae3c320fbf0
J05h <J05h@protonmail.com>
2020-11-23 21:22:17 -0500

    updates

--- CC_Qs/BloombergPractice/decodeString.py
+++ CC_Qs/BloombergPractice/decodeString.py
@@ -1,12 +1,64 @@
 # https://leetcode.com/problems/decode-string/
 
+# use two stacks or recursion
 
 class Solution:
-    def func(self, param):
-        return None
+
+    def decodeString(self, s):
+        currS = ""
+        k = ""
+        cStk = []
+        sStk = []
+
+        for cIdx in range(len(s)):
+            char = s[cIdx]
+            if char in '0123456789':
+                k += char
+            elif char in "[":
+                cStk.append(int(k))
+                sStk.append(currS)
+                k = ""
+                currS = ""
+            elif char in "]":
+                poppedS = sStk[-1]
+                sStk.pop(-1)
+                poppedK = cStk[-1]
+                cStk.pop(-1)
+                poppedS += (currS * poppedK)
+                currS = poppedS
+            else:
+                currS += char
+
+        return currS
+
+    k = [""]
+
+    def decodeString3(self, s):
+        string = ""
+
+        for cIdx in range(len(s)):
+            char = s[cIdx]
+            
+            if char in '0123456789':
+                self.k[-1] += char
+
+            elif char in "[":
+                string += self.decodeString(s[cIdx+1:])
+
+            elif char in "]":
+                if self.k[-1] != "":
+                    string *= int(self.k[-1]) - 1
+                    self.k.pop(-1)
+                    self.k.append("")
+                    return string
+                
+            else:
+                string += char
+
+        return string
 
 
 if __name__ == '__main__':
     tester = Solution()
-    ans = tester.func(None)
+    ans = tester.decodeString("3[a]2[bc]")
     print(ans)
